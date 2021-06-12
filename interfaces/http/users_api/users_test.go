fff639f951ef1d5caea170b94d57f9f8db3fdbf8
Allan Tony Selvan <allan.tony.selvan@nl.abnamro.com>
2021-06-12 14:05:47 +0200

    added test case for user store

--- interfaces/http/users_api/users_test.go
+++ interfaces/http/users_api/users_test.go
@@ -1,9 +1,9 @@
 package users_api
 
 import (
-	"fmt"
 	"github.com/go-resty/resty/v2"
 	"github.com/jarcoal/httpmock"
+	"github.com/privatesquare/bkst-go-utils/utils/errors"
 	"github.com/privatesquare/bkst-oauth-api/domain"
 	"github.com/stretchr/testify/assert"
 	"net/http"
@@ -19,13 +19,16 @@ var (
 		Password: "password",
 	}
 
-	user = domain.User{
+	userResp = domain.User{
 		Id:        1,
 		FirstName: "Harry",
 		Lastname:  "Potter",
 		Email:     "hpotter@hogwarts.com",
 		Status:    "active",
 	}
+
+	userNotFoundRestErr       = errors.NotFoundError("User with email notfound@hogwarts.com was not found")
+	invalidCredentialsRestErr = errors.BadRequestError("username or password is incorrect")
 )
 
 func init() {
@@ -37,18 +40,72 @@ func TestMain(m *testing.M) {
 	os.Exit(m.Run())
 }
 
-func TestUsersStore_Login_ResponseErr(t *testing.T) {
+func TestNewUsersStore(t *testing.T) {
+	us := NewUsersStore(resty.New())
+	assert.NotNil(t, us)
+}
+
+func TestUsersStore_Login(t *testing.T) {
 	client := resty.New().SetHostURL(baseUrl)
 	httpmock.ActivateNonDefault(client.GetClient())
 	defer httpmock.DeactivateAndReset()
 
-	responder, err := httpmock.NewJsonResponder(http.StatusOK, &user)
+	responder, err := httpmock.NewJsonResponder(http.StatusOK, &userResp)
 	assert.NoError(t, err)
 	httpmock.RegisterResponder("POST", baseUrl+usersLoginApiPath, responder)
 
 	us := NewUsersStore(client)
 	user, restErr := us.Login(login)
 
-	fmt.Println(user)
-	fmt.Println(restErr)
+	assert.Nil(t, restErr)
+	assert.Equal(t, userResp.Id, user.Id)
+	assert.Equal(t, userResp.FirstName, user.FirstName)
+	assert.Equal(t, userResp.Lastname, user.Lastname)
+	assert.Equal(t, userResp.Email, user.Email)
+	assert.Equal(t, userResp.Status, user.Status)
+}
+
+func TestUsersStore_Login_RequestError(t *testing.T) {
+	client := resty.New().SetHostURL(baseUrl)
+	us := NewUsersStore(client)
+	user, restErr := us.Login(login)
+
+	assert.Nil(t, user)
+	assert.Equal(t, http.StatusInternalServerError, restErr.Status)
+}
+
+func TestUsersStore_Login_UserNotFound(t *testing.T) {
+	client := resty.New().SetHostURL(baseUrl)
+	httpmock.ActivateNonDefault(client.GetClient())
+	defer httpmock.DeactivateAndReset()
+
+	responder, err := httpmock.NewJsonResponder(http.StatusNotFound, &userNotFoundRestErr)
+	assert.NoError(t, err)
+	httpmock.RegisterResponder("POST", baseUrl+usersLoginApiPath, responder)
+
+	us := NewUsersStore(client)
+	user, restErr := us.Login(login)
+
+	assert.Nil(t, user)
+	assert.Equal(t, userNotFoundRestErr.Message, restErr.Message)
+	assert.Equal(t, userNotFoundRestErr.Status, restErr.Status)
+	assert.Equal(t, userNotFoundRestErr.Error, restErr.Error)
+}
+
+func TestUsersStore_Login_InvalidCredentials(t *testing.T) {
+	client := resty.New().SetHostURL(baseUrl)
+	httpmock.ActivateNonDefault(client.GetClient())
+	defer httpmock.DeactivateAndReset()
+
+	responder, err := httpmock.NewJsonResponder(http.StatusNotFound, &invalidCredentialsRestErr)
+	assert.NoError(t, err)
+	httpmock.RegisterResponder("POST", baseUrl+usersLoginApiPath, responder)
+
+	us := NewUsersStore(client)
+	user, restErr := us.Login(login)
+
+	assert.Nil(t, user)
+	assert.Equal(t, invalidCredentialsRestErr.Message, restErr.Message)
+	assert.Equal(t, invalidCredentialsRestErr.Status, restErr.Status)
+	assert.Equal(t, invalidCredentialsRestErr.Error, restErr.Error)
 }
